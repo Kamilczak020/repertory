33e4e19547f81fef8d2f612875dbc4255f2a5223
Kamil Solecki <kamilczak020@gmail.com>
2019-07-04 16:37:27 +0200

    Added Authentication router

---
+++ backend/src/router/authRouter.ts
@@ -0,0 +1,20 @@
+import { Router } from 'express';
+import { apiMethod } from '../util/apiMethod';
+import { loginUser, registerUser } from '../controllers/userController';
+
+export class AuthRouter {
+  public router: Router;
+
+  constructor() {
+    this.router = Router();
+    this.init();
+  }
+
+  private init() {
+    this.router.post('/register', apiMethod(registerUser));
+    this.router.post('/login', apiMethod(loginUser));
+  }
+}
+
+const authRouter = new AuthRouter();
+export default authRouter.router;
