262994aa32c583dea8e7ca8dc1afedd4288155fd
Kamil Solecki <kamilczak020@gmail.com>
2019-07-04 16:34:14 +0200

    Added new views to routes

--- frontend/src/app/index.tsx
+++ frontend/src/app/index.tsx
@@ -1,15 +1,23 @@
-import { RepertoryApp } from 'app/containers/RepertoryApp';
-import { Root } from 'app/containers/Root';
 import * as React from 'react';
+import { LoginPage } from 'app/containers/LoginPage';
+import { MainPage } from 'app/containers/MainPage';
+import { RegisterPage } from './containers/RegisterPage';
+import { Root } from 'app/containers/Root';
 import { hot } from 'react-hot-loader/root';
 import { Route, Router, Switch } from 'react-router';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faCheckCircle, faTimesCircle } from '@fortawesome/free-regular-svg-icons';
+
+library.add(faCheckCircle, faTimesCircle);
 
 // render react DOM
 export const app = hot(({ history }) => (
   <Root>
     <Router history={history}>
       <Switch>
-        <Route path="/" component={RepertoryApp} />
+        <Route path="/watch" component={MainPage} />
+        <Route path="/signin" component={LoginPage} />
+        <Route path="/register" component={RegisterPage} />
       </Switch>
     </Router>
   </Root>
