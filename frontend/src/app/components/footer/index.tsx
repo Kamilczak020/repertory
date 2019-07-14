d6b416d1b878b7c9b26fa13757bae6c9459ab725
Kamilczak020 <kamilczak020@gmail.com>
2019-07-14 22:04:55 +0200

    Fixed footer layout

--- frontend/src/app/components/footer/index.tsx
+++ frontend/src/app/components/footer/index.tsx
@@ -16,19 +16,32 @@ export class Footer extends React.Component {
         <div className={style.footerContent}>
           <div className={style.contentGroup}>
             <h4>General</h4>
-            <a onClick={() => routerStore.push('/')}>Home</a>
-            <a onClick={() => routerStore.push('/')}>Watch</a>
-            <a onClick={() => routerStore.push('/')}>Terms of Service</a>
-            <a onClick={() => routerStore.push('/')}>Privacy Policy</a>
+            <div className={style.links}>
+              <div className={style.linkGroup}>
+                <a onClick={() => routerStore.push('/')}>Home</a>
+                <a onClick={() => routerStore.push('/watch')}>Watch</a>
+              </div>
+              <div className={style.linkGroup}>
+                <a onClick={() => routerStore.push('/tos')}>Terms of Service</a>
+                <a onClick={() => routerStore.push('/privacy-policy')}>Privacy Policy</a>
+              </div>
+            </div>
           </div>
           <div className={style.contentGroup}>
             <h4>Account</h4>
-            <a onClick={() => routerStore.push('/')}>Sign In</a>
-            <a onClick={() => routerStore.push('/')}>Register</a>
-            <a onClick={() => routerStore.push('/')}>Profile</a>
-            <a onClick={() => routerStore.push('/')}>Settings</a>
+            <div className={style.links}>
+              <div className={style.linkGroup}>
+                <a onClick={() => routerStore.push('/signin')}>Sign In</a>
+                <a onClick={() => routerStore.push('/register')}>Register</a>
+              </div>
+              <div className={style.linkGroup}>
+                <a onClick={() => routerStore.push('/profile')}>Profile</a>
+                <a onClick={() => routerStore.push('/')}>Settings</a>
+              </div>
+            </div>
           </div>
           <div className={style.contentGroup}>
+            <h4>About</h4>
             <a href="https://github.com/Kamilczak020/repertory" className={style.linkField}>
               <Github className={style.icon} viewBox="0 0 438.549 438.549"/>
               <p>Kamilczak020</p>
