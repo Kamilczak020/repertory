a79a49199253ef79a7fa38fa5f25862984cf08c6
Kamil Solecki <kamilczak020@gmail.com>
2019-07-24 16:21:48 +0200

    Added header handling

--- frontend/src/app/api/index.ts
+++ frontend/src/app/api/index.ts
@@ -2,9 +2,15 @@ import axios from 'axios';
 import * as qs from 'qs';
 
 export abstract class API {
-  public static post(route: string, body: object) {
+  public static post(route: string, body?: object) {
     const uri = 'http://localhost:3001';
 
-    return axios.post(uri + route, qs.stringify(body), { responseType: 'json', headers: { 'content-type': 'application/x-www-form-urlencoded' }});
+    return axios.post(uri + route, qs.stringify(body), {
+      responseType: 'json',
+      withCredentials: true,
+      headers: {
+        'content-type': 'application/x-www-form-urlencoded',
+      },
+    });
   }
 }
