2d1a1e8bb5b625b0dc868c7c3d4275529b219116
Kamil Solecki <kamilczak020@gmail.com>
2019-07-24 16:24:26 +0200

    Added auth HoC

---
+++ frontend/src/app/util/authenticated/index.tsx
@@ -0,0 +1,30 @@
+import * as React from 'react';
+import { RouteComponentProps } from 'react-router-dom';
+import { inject, observer } from 'mobx-react';
+import { STORE_ROUTER, STORE_USER } from 'app/constants';
+import { RouterStore, UserStore } from 'app/stores';
+
+export function authenticated(BaseComponent: React.ComponentClass) {
+  @inject(STORE_ROUTER, STORE_USER)
+  @observer
+  class AuthenticatedComponent extends React.Component<RouteComponentProps> {
+    public componentWillMount() {
+      this.checkAuthentication();
+    }
+
+    public async checkAuthentication() {
+      const routerStore = this.props[STORE_ROUTER] as RouterStore;
+      const userStore = this.props[STORE_USER] as UserStore;
+
+      if (!userStore.isAuthenticated) {
+        routerStore.replace('/signin');
+      }
+    }
+
+    public render() {
+      return <BaseComponent {...this.props} />;
+    }
+  }
+
+  return AuthenticatedComponent;
+}
