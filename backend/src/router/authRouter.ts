9f0449905b968500de24b1ff8b0f44362676344e
Kamil Solecki <kamilczak020@gmail.com>
2019-07-26 15:20:09 +0200

    Added validate path

--- backend/src/router/authRouter.ts
+++ backend/src/router/authRouter.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import { apiMethod } from '../util/apiMethod';
-import { loginUser, registerUser, verifyUser } from '../controller/userController';
+import { loginUser, logoutUser, registerUser, verifyUser } from '../controller/userController';
 
 export class AuthRouter {
   public router: Router;
@@ -13,6 +13,7 @@ export class AuthRouter {
   private init() {
     this.router.post('/register', apiMethod(registerUser));
     this.router.post('/login', apiMethod(loginUser));
+    this.router.post('/logout', apiMethod(logoutUser));
     this.router.post('/verify', apiMethod(verifyUser));
   }
 }
