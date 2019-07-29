3096c0fbdbfa0a0399bef87cdc9ae48ae3867d56
Kamil Solecki <kamilczak020@gmail.com>
2019-07-29 16:07:09 +0200

    Created generic progress bar component

---
+++ frontend/src/app/components/progressBar/index.tsx
@@ -0,0 +1,26 @@
+import * as React from 'react';
+import * as classnames from 'classnames/bind';
+import * as style from './style.css';
+
+const cx = classnames.bind(style);
+
+export interface ProgressBarProps {
+  className?: string;
+  value: number;
+  color: string;
+}
+
+export class ProgressBar extends React.Component<ProgressBarProps> {
+  public render() {
+    const progressBarClassnames = cx({
+      progressBar: true,
+      [this.props.className]: true
+    });
+
+    return (
+      <div className={progressBarClassnames}>
+        <div className={style.progress} style={{ width: `${this.props.value}%`, backgroundColor: this.props.color }} />
+      </div>
+    );
+  }
+}
