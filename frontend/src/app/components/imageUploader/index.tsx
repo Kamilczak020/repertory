a478bce30721089644718cb6e3978b7b7a13221b
Kamil Solecki <kamilczak020@gmail.com>
2019-07-29 16:06:12 +0200

    removed generic ModalWindow

--- frontend/src/app/components/imageUploader/index.tsx
+++ frontend/src/app/components/imageUploader/index.tsx
@@ -1,11 +1,11 @@
 import * as React from 'react';
 import * as style from './style.css';
 import * as classnames from 'classnames/bind';
-import { STORE_ROUTER } from 'app/constants';
+import { STORE_ROUTER, STORE_UI } from 'app/constants';
 import { inject, observer } from 'mobx-react';
 import PlaceholderImage from '../../../assets/images/placeholder-user.jpg';
-import { ModalWindow } from '../modalWindow';
 import { ImageUploaderModal } from '../imageUploaderModal';
+import { UIStore } from 'app/stores/uiStore';
 
 const cx = classnames.bind(style);
 
@@ -13,10 +13,12 @@ export interface ImageUploaderProps {
   className?: string;
 }
 
-@inject(STORE_ROUTER)
+@inject(STORE_ROUTER, STORE_UI)
 @observer
 export class ImageUploader extends React.Component<ImageUploaderProps> {
   public render() {
+    const uiStore = this.props[STORE_UI] as UIStore;
+
     const imageUploaderContainerClassnames = cx({
       imageUploaderContainer: true,
       [this.props.className]: true,
@@ -25,10 +27,8 @@ export class ImageUploader extends React.Component<ImageUploaderProps> {
     return (
       <div className={imageUploaderContainerClassnames}>
         <img className={style.profilePicture} src={PlaceholderImage} />
-        <button>Change Image</button>
-        <ModalWindow>
-          <ImageUploaderModal />
-        </ModalWindow>
+        <button onClick={() => uiStore.imageModalOpen = true}>Change Image</button>
+        <ImageUploaderModal />
       </div>
     );
   }
