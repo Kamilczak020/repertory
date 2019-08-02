5b181c453bbdd805167e6c6c2b9d3f47ce1014b1
Kamil Solecki <kamilczak020@gmail.com>
2019-08-02 16:01:15 +0200

    Finished bday modal

--- frontend/src/app/components/modals/birthdayModal/index.tsx
+++ frontend/src/app/components/modals/birthdayModal/index.tsx
@@ -4,17 +4,50 @@ import { BaseModal } from '../baseModal';
 import { inject, observer } from 'mobx-react';
 import { STORE_PROFILE } from 'app/constants';
 import { ProfileStore } from 'app/stores';
+import { format } from 'date-fns';
+import { toast } from 'react-toastify';
+import Calendar from 'react-calendar';
+import '!style-loader!css-loader!./style-calendar-override.css';
+
+export interface BirthdayModalState {
+  selectedDate: Date;
+}
 
 @inject(STORE_PROFILE)
 @observer
-export class BirthdayModal extends React.Component {
+export class BirthdayModal extends React.Component<{}, BirthdayModalState> {
+  public state = {
+    selectedDate: new Date(Date.now())
+  }
+
+  private async handleSave() {
+    const profileStore = this.props[STORE_PROFILE] as ProfileStore;
+    
+    try {
+      await profileStore.saveBirthday(this.state.selectedDate);
+      toast.success('Location updated');
+    } catch (error) {
+      toast.error('Could not update location');
+    }
+  }
+
   public render() {
     const profileStore = this.props[STORE_PROFILE] as ProfileStore;
 
     return (
       <BaseModal className={style.birthdayModal} isOpen={profileStore.birthdayModalOpen} title="Change Birthday"
         onClose={() => profileStore.birthdayModalOpen = false}
-        onSave={() => null}>
+        onSave={() => this.handleSave()}>
+        <Calendar className={style.calendar}
+          calendarType="US"
+          locale="en-US"
+          minDate={new Date('01-01-1900')}
+          maxDate={new Date(Date.now())} 
+          formatShortWeekday={(locale, date) => format(date, 'dd', { locale }).charAt(0)}
+          formatMonth={(locale, date) => format(date, 'MMM', { locale })}
+          onChange={(selectedDate: Date) => this.setState({ selectedDate })}
+          value={this.state.selectedDate}
+        />
       </BaseModal>
     );
   }
